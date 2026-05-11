562d8cd69d072b80e2feaf2a0d22db87e29b32c2
Emosaddd <pl.maksim.krukovich@gmail.com>
2026-05-11 12:51:55 +0200

    feat(network-config): add pharos network configuration

---
+++ apps/web/src/config/templateConfig.ts
@@ -0,0 +1,20 @@
+import type { TemplateConfig } from './constants.extra'
+
+/**
+ * THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
+ * Generated from /networks/pharos/config.json
+ */
+const TEMPLATE_CONFIG = {
+  EIP155: false,
+  SUPPORTED_VERSIONS: ['1.3.0', '1.4.1'],
+  LOGO_DIMENSIONS: {
+    HEADER: {
+      H: '45px',
+    },
+    WELCOME: {
+      W: '160px',
+      H: '48px',
+    },
+  },
+} as TemplateConfig
+export default TEMPLATE_CONFIG
